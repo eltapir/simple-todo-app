6a8a6c9120d54e7affe08a77d330b2c6f4a72a85
Kris HEYSE <kris@krisheyse.org>
2020-04-06 10:35:50 +0200

    Updates

---
+++ precache-manifest.a51e8fde7131dd4d139a01cbb907a995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f1f87ec9f5a58ec40e6d15f2646f6f",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "9b86ff8e12915f8951de",
+    "url": "/simple-todo-app/static/css/main.174b77fb.chunk.css"
+  },
+  {
+    "revision": "b7f2d396c85d70b19d25",
+    "url": "/simple-todo-app/static/js/2.74ab088f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.74ab088f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b86ff8e12915f8951de",
+    "url": "/simple-todo-app/static/js/main.424dbbf3.chunk.js"
+  },
+  {
+    "revision": "7b423f3aba4c20de5096",
+    "url": "/simple-todo-app/static/js/runtime-main.8d73543e.js"
+  }
+]);
